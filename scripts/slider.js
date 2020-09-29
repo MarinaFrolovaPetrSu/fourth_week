827339249b0186ea327b2b6f84ec321b49b6d7d5
Marina Frolova <frolova23m@gmail.com>
2020-09-29 04:21:24 +0300

    week_4

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,4 +1,14 @@
-$('.products').bxSlider({
+const slider = $('.products').bxSlider({
   pager: false,
   controls: false
 });
+
+$('.products-slider__arrow--back').click (e => {
+  e.preventDefault();
+  slider.goToPrevSlide();
+})
+
+$('.products-slider__arrow--next').click (e => {
+  e.preventDefault();
+  slider.goToNextSlide();
+})
