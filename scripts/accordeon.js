d5aa7fd6672a65f3fb87d1764c9dab6eb4bf1c20
Marina Frolova <frolova23m@gmail.com>
2020-10-02 01:10:12 +0300

    week_4

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -1,64 +1,46 @@
-const verticalAccordeon = () => {
-  const links = document.querySelectorAll(".products-menu__title");
-  const body = document.querySelector('body');
+const measureWidth = (item) => {
+  const screenWidth = $(window).width();
+  const container = item.closest(".products-menu");
+  const titleBlocks = container.find(".products-menu__title");
+  const titleWidth = titleBlocks.width() * titleBlocks.length;
 
-  const calculateWidth = () => {
-    const windowWidth = window.innerWidth;
-
-    const MAX_WIDTH = 550;
-
-    const linksWidth = links[0].offsetWidth;
-    console.log(linksWidth);
-
-    const reqWidth = windowWidth - (linksWidth * links.length);
-
-    return reqWidth > MAX_WIDTH ? MAX_WIDTH : reqWidth;
-  };
-
-  function closeItem(activeElement) {
-    const activeText = activeElement.querySelector(".products-menu__content");
-    activeText.style.width = "0px";
-    activeElement.classList.remove("active");
-  }
-
-  links.forEach(function (elem) {
-    elem.addEventListener("click", function (e) {
-      e.preventDefault();
-      const link = e.target.closest(".products-menu__title");
-      const active = document.querySelector(".products-menu__title.active");
-      if (active) {
-        closeItem(active);
-      } 
-
-      if (!active || active.querySelector(".products-menu__title") !== link) {
-        const current = link.closest(".products-menu__item");
-        current.classList.add("active");
-        const currentText = current.querySelector(".products-menu__content");
-        if (body.offsetWidth > 480) {
-          currentText.style.width = calculateWidth() + 'px';
-
-          widthEl = calculateWidth();
-          console.log(widthEl);
-        } else {
-          currentText.style.width = '100%';
-        }
-      }
-    });
-  });
-  
-  document.addEventListener("click", (e) => {
-    e.preventDefault();
-    let activePerson = document.querySelector(".products-menu__item.active");
-    const target = e.target;
-    console.log(target);
-    if (!target.closest(".products-menu") && activePerson) {
-      closeItem(activePerson);
-    }
-    if (target.closest(".products-menu__close") && activePerson) {
-      closeItem(activePerson);
-    }
-  });
-
+  return screenWidth - titleWidth;
 };
 
-verticalAccordeon();
+const closeEveryItemInContainer = (container) => {
+  const items = container.find(".products-menu__item");
+  const content = container.find(".products-menu__content");
+
+  items.removeClass("active");
+  content.width(0);
+};
+
+const openItm = (item) => {
+  const hiddenContent = item.find(".products-menu__content");
+  const reqWidth = measureWidth(item);
+
+  item.addClass("active");
+  hiddenContent.width(reqWidth);
+};
+
+$(".products-menu__title").on("click", (e) => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const item = $this.closest(".products-menu__item");
+  const itemOpened = item.hasClass("active");
+  const container = $this.closest(".products-menu");
+
+  if (itemOpened) {
+    closeEveryItemInContainer(container)
+  } else {
+    closeEveryItemInContainer(container)
+    openItm(item);
+  }
+});
+
+$(".products-menu__close").on("click", (e) => {
+  e.preventDefault();
+
+  closeEveryItemInContainer($('.products-menu'));
+})
